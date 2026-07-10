ccc3419ca147bc62c2a7fb3afb11ec6bad10e8c7
Jose Sanchez <leandrosanchez.it@gmail.com>
2026-07-10 19:13:40 -0400

    feat: Added a label to display a message of course complete

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,10 +1,16 @@
 extends Node
 
-var score: int = 0
 @onready var coins_collected: Label = $CoinsCollected
+@onready var congratulations: Label = $Congratulations
 
+var score: int = 0
+var allCoins := false
 
 func add_point():
 	score += 1
-	coins_collected.text = "You collected " + str(score) + " coins"
-	print(score)
+	if score == 16:
+		allCoins = true
+	if allCoins:
+		congratulations.visible = true
+	coins_collected.text = "You collected " + str(score) + "/16 coins"
+	#print(score)
